f4b7c1f171be8109a340ab69e678f04fb67b64dc
AlexCsorba <124453983+AlexCsorba@users.noreply.github.com>
2023-10-24 15:47:02 -0400

    lex done

--- src/spellChecker/Milestone2.java
+++ src/spellChecker/Milestone2.java
@@ -1,9 +1,13 @@
 package spellChecker;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 public class Milestone2 {
 
     public Milestone2() {
@@ -24,7 +28,13 @@ public class Milestone2 {
         String word = reader.readLine();
 
         // Assuming lexicon is a List of words in the English language
-        List<String> lexicon = ...;
+        ArrayList<String> lexicon = new ArrayList<>();
+        File file = new File("resources/lexicon.txt");
+        Scanner fileScan = new Scanner(file);
+
+        while(fileScan.hasNextLine()) {
+            lexicon.add(fileScan.nextLine());
+        }
 
         SpellChecker spellChecker = new SpellChecker(lexicon);
 
